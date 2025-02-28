1c8a44e1542bd4c1d87f29d83a016a724c34f04f
AirTMZ <50797752+AirTMZ@users.noreply.github.com>
2025-02-28 20:28:42 +0000

    Fixed Image Scaling

--- RandoSeed/RandoSeed/MainApp.Designer.cs
+++ RandoSeed/RandoSeed/MainApp.Designer.cs
@@ -29,38 +29,25 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainApp));
-            this.Reference = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.Reference)).BeginInit();
             this.SuspendLayout();
-            // 
-            // Reference
-            // 
-            this.Reference.Image = ((System.Drawing.Image)(resources.GetObject("Reference.Image")));
-            this.Reference.Location = new System.Drawing.Point(0, 0);
-            this.Reference.Name = "Reference";
-            this.Reference.Size = new System.Drawing.Size(600, 685);
-            this.Reference.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.Reference.TabIndex = 1;
-            this.Reference.TabStop = false;
             // 
             // MainApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(600, 685);
-            this.Controls.Add(this.Reference);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainApp";
             this.Text = "MainApp";
             this.Load += new System.EventHandler(this.MainApp_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.Reference)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox Reference;
     }
 }
 
--- RandoSeed/RandoSeed/MainApp.cs
+++ RandoSeed/RandoSeed/MainApp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,44 @@ using System.Windows.Forms;
 
 namespace RandoSeed
 {
-    public partial class MainApp: Form
+    public partial class MainApp : Form
     {
         public MainApp()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Load += MainApp_Load;
         }
 
         private void MainApp_Load(object sender, EventArgs e)
         {
+            this.Region = new Region(GetRoundedRectPath(this.ClientRectangle, 25));
+        }
 
+        private GraphicsPath GetRoundedRectPath(Rectangle rect, int radius)
+        {
+            int diameter = radius * 2;
+            Size size = new Size(diameter, diameter);
+            Rectangle arc = new Rectangle(rect.Location, size);
+            GraphicsPath path = new GraphicsPath();
+
+            // top left arc
+            path.AddArc(arc, 180, 90);
+
+            // top right arc
+            arc.X = rect.Right - diameter;
+            path.AddArc(arc, 270, 90);
+
+            // bottom right arc
+            arc.Y = rect.Bottom - diameter;
+            path.AddArc(arc, 0, 90);
+
+            // bottom left arc
+            arc.X = rect.Left;
+            path.AddArc(arc, 90, 90);
+
+            path.CloseFigure();
+            return path;
         }
     }
 }
